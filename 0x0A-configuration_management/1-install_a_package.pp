db40fd6c5fcbea7d8f12528b05e7248a29b13829
paulsikabalu <sikabalu@outlook.com>
2023-11-25 18:39:20 +0200

    conf management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,23 @@ package { 'flask':
   ensure  => '2.1.0',
   provider  => 'pip3',
 }
+
+exec { 'install ruby':
+  command => '/usr/bin/apt-get install -y ruby'
+}
+
+exec { 'install puppet-lint':
+  command => '/usr/bin/gem install puppet-lint -v 2.1.1'
+}
+
+package { 'ruby':
+  ensure => 'installed',
+  before => Exec['install ruby']
+}
+
+package { 'puppet-lint':
+  ensure  => 'installed',
+  before  => Exec['install puppet-lint'],
+  require => Package['ruby']
+}
+
